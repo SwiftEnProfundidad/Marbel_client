4cfc9ac1f3b3a44677e5ad7db90efc2f2c7d6c2d
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-03-17 00:32:30 +0100

    Refactor

--- OpenBankMarvelTests/Helpers.swift
+++ OpenBankMarvelTests/Helpers.swift
@@ -10,13 +10,6 @@ import Foundation
 @testable import OpenBankMarvel
 
 extension XCTestCase {
-//    func verify<T>(url: String, in fetchableValue: FetchableValue<T>, file: StaticString = #file, line: UInt = #line) {
-//        XCTAssertEqual(url, fetchableValue.url.absoluteString, file: file, line: line)
-//        if case .fetched(_) = fetchableValue.value {
-//            XCTFail("A decoded FetchableValue should have an unfetched value", file: file, line: line)
-//        }
-//    }
-    
     func openJsonFile<Model: Decodable>(withName name: String) -> Model? {
         let bundle = Bundle(for: type(of: self))
         guard let url = bundle.url(forResource: name, withExtension: "json"),
@@ -24,6 +17,7 @@ extension XCTestCase {
             XCTFail("Could not load the data from the file")
             return nil
         }
+        
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         guard let model = try? decoder.decode(Model.self, from: data) else {
